ab3e1652204d7d7c963c2746aeb4adfcd732da32
ravin thambapillai <rthambapillai@palantir.com>
2015-05-07 14:57:07 +0100

    initial commit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,6 @@
+class ProjectsController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  def overview
+  end
+end
