7464fb581f86c8b5d6d4ee0a6a70f4bc99fddffd
ravin thambapillai <rthambapillai@palantir.com>
2015-05-07 18:21:36 +0100

    add description to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507171350) do
+ActiveRecord::Schema.define(version: 20150507172027) do
 
   create_table "projects", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "value"
     t.integer  "urgency"
+    t.text     "description"
   end
 
   create_table "tasks", force: :cascade do |t|
