8f2838a98f957be57a03869bc5033bc89eeec59b
ravin thambapillai <rthambapillai@palantir.com>
2015-06-23 12:24:47 +0100

    christ fucking knows

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,8 +2,22 @@ class ProjectsController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   def overview
+  	@project = Project.new
   end
+
   def show
   	@project = Project.find(params[:id])
   end
+
+  def create
+  	@project = Project.new(project_params)
+  	@project.save
+  	redirect_to root_path
+  end
+
+  private 
+
+  	def project_params
+  		params.require(:project).permit(:description, :urgency, :value)
+  	end
 end
